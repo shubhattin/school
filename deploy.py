52ee82c3015212d99f55bfa2822f437053658cb5
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2022-11-03 13:48:02 +0530

    karah.py

--- deploy.py
+++ deploy.py
@@ -1,12 +1,12 @@
 import shubhlipi as sh
 import os
 
-sh.cmd('python cpy.py')
+sh.cmd('python karah.py')
 if sh.cmd('git switch pages')[0] == 0:
     for x in os.listdir('./'):
         if x not in ["build", ".git"]:
             sh.delete(x)
-    for x in [".gitignore", 'cpy.py', 'deploy.py', 'javatool.py', 'README.md', "proj", "algo"]:
+    for x in [".gitignore", 'karah.py', 'deploy.py', 'javatool.py', 'README.md', "proj", "algo"]:
         sh.delete(f'build/{x}')
     sh.copy_folder('./build', './')
     sh.delete_folder('./build')
